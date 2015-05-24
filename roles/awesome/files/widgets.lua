d969a82c143f7323c0ac28755345c07285eb48a6
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-05-24 15:55:34 +0200

    updated awesome audio widget

--- roles/awesome/files/widgets.lua
+++ roles/awesome/files/widgets.lua
@@ -23,30 +23,25 @@ volume_widget = wibox.widget.textbox()
 volume_widget:set_align("right")
 
 function update_volume(widget)
-   local fd = io.popen("amixer sget Master")
-   local status = fd:read("*all")
-   fd:close()
+    local fd = io.popen("amixer sget Master")
+    local status = fd:read("*all")
+    fd:close()
 
-   local volume = tonumber(string.match(status, "(%d?%d?%d)%%")) / 100
-   -- volume = string.format("% 3d", volume)
+    -- local volume = tonumber(string.match(status, "(%d?%d?%d)%%")) / 100
+    local volume = string.match(status, "(%d?%d?%d)%%")
+    volume = string.format("% 3d", volume)
 
-   status = string.match(status, "%[(o[^%]]*)%]")
+    status = string.match(status, "%[(o[^%]]*)%]")
 
-   -- starting colour
-   local sr, sg, sb = 0x3F, 0x3F, 0x3F
-   -- ending colour
-   local er, eg, eb = 0xDC, 0xDC, 0xCC
+    if string.find(status, "on", 1, true) then
+        -- For the volume numbers
+        volume = volume .. "%"
+    else
+        -- For the mute button
+        volume = volume .. "M"
+    end
 
-   local ir = volume * (er - sr) + sr
-   local ig = volume * (eg - sg) + sg
-   local ib = volume * (eb - sb) + sb
-   interpol_colour = string.format("%.2x%.2x%.2x", ir, ig, ib)
-   if string.find(status, "on", 1, true) then
-       volume = " <span background='#" .. interpol_colour .. "'>   </span>"
-   else
-       volume = " <span color='red' background='#" .. interpol_colour .. "'> M </span>"
-   end
-   widget:set_markup(volume)
+    widget:set_markup(volume)
 end
 
 update_volume(volume_widget)
