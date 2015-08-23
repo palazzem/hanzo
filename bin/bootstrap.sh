caee389e785c0d092a5f22032017443f1ede4360
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-08-24 00:10:36 +0200

    Awesome role is included in the main orchestrate.yml; removing a separated installation

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -54,7 +54,6 @@ fi
 # Proceeding with orchestration
 echo "Starting orchestration..."
 ansible-playbook orchestrate.yml -i inventory --connection=local -e "fullname='$FULLNAME' email=$EMAIL username=$USERNAME"
-ansible-playbook awesome.yml -i inventory --connection=local -e "username=$USERNAME"
 
 echo "Configuration completed!"
 echo "The following command is *mandatory*:"
