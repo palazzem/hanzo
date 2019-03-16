ec453eb9da4fb80c0deca930898999ff95b16320
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2019-03-16 19:36:06 +0100

    [license] update license year

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Copyright (c) 2014, Emanuele Palazzetti and contributors
+# Copyright (c) 2014-2019, Emanuele Palazzetti and contributors
 # All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
