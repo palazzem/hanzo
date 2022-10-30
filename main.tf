6922b128fff8f72a8b556ccc6a77b20b4a99a844
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2022-10-30 14:20:41 +0100

    Remove Git configuration (dotfiles are used)

--- main.tf
+++ main.tf
@@ -50,12 +50,6 @@ resource "coder_agent" "main" {
   # workspace. Note that they take precedence over configuration defined in ~/.gitconfig!
   # You can remove this block if you'd prefer to configure Git manually or using
   # dotfiles. (see docs/dotfiles.md)
-  env = {
-    GIT_AUTHOR_NAME     = "${data.coder_workspace.me.owner}"
-    GIT_COMMITTER_NAME  = "${data.coder_workspace.me.owner}"
-    GIT_AUTHOR_EMAIL    = "${data.coder_workspace.me.owner_email}"
-    GIT_COMMITTER_EMAIL = "${data.coder_workspace.me.owner_email}"
-  }
 }
 
 resource "coder_app" "code-server" {
