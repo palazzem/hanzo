ef23e6efe5d743fe04e2c9c32685d87dd6e7846a
Manu Palazzetti <emanuele.palazzetti@gmail.com>
2023-03-14 22:09:32 +0100

    fix: remove Ansible portable in favor of system package

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -30,8 +30,6 @@
 set -e
 
 # Variables
-ANSIBLE_VERSION=2.14.3
-ANSIBLE_FOLDER="ansible-$ANSIBLE_VERSION"
 REPOSITORY=https://github.com/palazzem/hanzo.git
 OUT_FOLDER=/root/.hanzo/
 
@@ -58,24 +56,21 @@ fi
 
 cd "$OUT_FOLDER"
 
-# Install Ansible Portable if not available
-if [ ! -d "$OUT_FOLDER/$ANSIBLE_FOLDER" ]; then
-    echo "Installing Ansible Portable..."
-    pacman -Sy tar python --noconfirm
-    curl -L https://github.com/palazzem/ansible-portable/releases/download/$ANSIBLE_VERSION/ansible-$ANSIBLE_VERSION.tar.gz > /tmp/ansible.tar.gz
+# Install Ansible
+if [ ! -d "$OUT_FOLDER/library" ]; then
+    echo "Installing Ansible..."
+    pacman -Sy tar ansible --noconfirm
     curl -L https://github.com/kewlfft/ansible-aur/archive/v0.24.tar.gz > /tmp/aur.tar.gz
-    tar -xf /tmp/ansible.tar.gz
     tar -xf /tmp/aur.tar.gz -C /tmp
     mkdir library
     mv /tmp/ansible-aur-0.24/aur.py ./library
-    ln -s ansible $ANSIBLE_FOLDER/ansible-playbook
 else
-    echo "Ansible found in $OUT_FOLDER/$ANSIBLE_FOLDER, skipping installation..."
+    echo "Ansible extensions found in $OUT_FOLDER/library, skipping installation..."
 fi
 
 # Orchestration
 echo "Starting Hanzo orchestration..."
-PYTHONPATH=$ANSIBLE_FOLDER python $ANSIBLE_FOLDER/ansible-playbook orchestrate.yml --connection=local --tags=$TAGS $EXTRA_ARGS
+ansible playbook orchestrate.yml --connection=local --tags=$TAGS $EXTRA_ARGS
 
 # Post-install script
 echo "Executing post-install steps..."
